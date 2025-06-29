9278a06198fbe64c837af6381be86551b4df9207
ItsOlegDm <itsolegdm@gmail.com>
2025-06-29 21:03:02 +0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
-  title: "RedTail",
-  description: "Команда переводчиков RedTail",
+    title: "RedTail",
+    description: "Команда переводчиков RedTail",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ru">
-      <body>
+    return (
+        <html lang="ru">
+        <head />
+        <body>
+        <Script
+            async
+            defer
+            data-website-id={process.env.PUBLIC_UMAMI_ID}
+            src="/umami.js"
+            strategy="afterInteractive"
+        />
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
