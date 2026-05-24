8aa2181bb0978bceb16baa0fded7cbf8ddccaec2
ItsOlegDm <itsolegdm@gmail.com>
2026-05-24 22:07:08 +0300

    fix: force dynamic rendering for teamcard

--- src/app/teamcard/page.tsx
+++ src/app/teamcard/page.tsx
@@ -3,6 +3,9 @@ import TeamCardClient from './TeamCardClient';
 
 const STRAPI_DOMAIN = process.env.PUBLIC_STRAPI_DOMAIN;
 
+export const dynamic = 'force-dynamic';
+
+
 export default async function TeamCardPage() {
     const team = await getTeamMembersAll();
     return <TeamCardClient team={team as any} strDomain={STRAPI_DOMAIN} />;
